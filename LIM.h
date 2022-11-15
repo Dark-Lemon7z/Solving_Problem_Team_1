cff0ee2ba98afa09d268e9caf6f7897ed1ae4eb5
sanggyun Lim <limsang7273@gachon.ac.kr>
2022-11-16 00:35:44 +0900

    1

--- LIM.h
+++ LIM.h
@@ -1,4 +1,19 @@
-struct test
-{
-	int key;
-};
+void delete(int key) {
+	Node* ptr = list_info.head;
+
+//첫 노드를 지우려는 경우
+	else if (list_info.head->tag == key) { 
+		list_info.head = list_info.head->next;
+		return;
+	}
+	else { 
+		while (ptr->next != NULL)
+		{
+			if (ptr->next->tag == key) { 
+				ptr->next = ptr->next->next;
+				return;
+			}
+			ptr = ptr->next;
+		}
+	}
+}
