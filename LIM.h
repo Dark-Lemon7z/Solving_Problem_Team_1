6af89c8ad5ec9309ad5185d19409c8979f2fe7ae
sanggyun Lim <limsang7273@gachon.ac.kr>
2022-11-16 00:38:27 +0900

    2

--- LIM.h
+++ LIM.h
@@ -1,12 +1,12 @@
 void delete(int key) {
 	Node* ptr = list_info.head;
 
-//첫 노드를 지우려는 경우
-	else if (list_info.head->tag == key) { 
+//첫 노드 지울 때
+	if (list_info.head->tag == key) { 
 		list_info.head = list_info.head->next;
 		return;
 	}
-	else { 
+	else if{ 
 		while (ptr->next != NULL)
 		{
 			if (ptr->next->tag == key) { 
