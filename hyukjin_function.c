a82320173738af3e43837956115827c0db0bedfe
realHyukjin <hjjang1201@gachon.ac.kr>
2022-11-16 17:03:31 +0900

    add comment

--- hyukjin_function.c
+++ hyukjin_function.c
@@ -1,5 +1,13 @@
 #include "hyukjin_header.h"
 
+/*
+ * Name        : search_tag
+ * Date        : 2022-11-14
+ * argument    : int tag
+ * return      : node *
+ * description : This function can find the tag from txt file.
+*/
+
 Node* serarch_tag(int tag)
 {
 	Node* ptr = list_info.head;
