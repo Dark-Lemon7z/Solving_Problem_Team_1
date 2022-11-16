599addaa7e2d79d3f99e94e71f2b43a3ca5a2a43
sanggyun Lim <limsang7273@gachon.ac.kr>
2022-11-16 13:57:36 +0900

    3

--- LIM_function.c
+++ LIM_function.c
@@ -1,15 +1,17 @@
+#include "LIM.h"
+
 void delete(int key) {
 	Node* ptr = list_info.head;
 
-//첫 노드 지울 때
-	if (list_info.head->tag == key) { 
+	//첫 노드 지울 때
+	if (list_info.head->tag == key) {
 		list_info.head = list_info.head->next;
 		return;
 	}
-	else if{ 
+	else if{
 		while (ptr->next != NULL)
 		{
-			if (ptr->next->tag == key) { 
+			if (ptr->next->tag == key) {
 				ptr->next = ptr->next->next;
 				return;
 			}
---
+++ LIM_header.h
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void delete(int key);
