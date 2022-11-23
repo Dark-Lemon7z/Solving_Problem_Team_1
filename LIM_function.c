83b0c8f76d0766f73cc6e001c38095e164115e06
sanggyun Lim <limsang7273@gachon.ac.kr>
2022-11-23 22:13:23 +0900

    add delete_name

--- LIM_function.c
+++ LIM_function.c
@@ -1,10 +1,11 @@
 #include "LIM_header.h"
 
 void delete(int key) {
-	Node* ptr = list_info.head;
 
-	//첫 노드 지울 때
-	if (list_info.head->tag == key) {
+	Node* ptr = list_info.head, * prev = list_info.head;
+
+	if (list_info.head->data.tag == key)
+	{
 		list_info.head = list_info.head->next;
 		free(ptr);
 		return;
@@ -21,6 +22,38 @@ void delete(int key) {
 				return;
 			}
 			ptr = ptr->next;
+			prev = prev->next;
 		}
 	}
+	printf("==No data exist==");
+
+}
+
+void delete_name(char key) {
+	Node* ptr = list_info.head, * prev = list_info.head;
+	while (ptr != NULL)
+	{
+		if (list_info.head->data.name == key)
+		{
+			list_info.head = list_info.head->next;
+			free(ptr);
+		}
+		else
+		{
+			ptr = ptr->next;
+			while (ptr != NULL)
+			{
+				if (ptr->data.name == key)
+				{
+					prev->next = ptr->next;
+					free(ptr);
+				}
+				ptr = ptr->next;
+				prev = prev->next;
+			}
+		}
+	}
+	printf("==No data exist==");
+
+
 }
